a8235cef1f583feb2a1db140fbe253a38c15764c
Morgane <morgane.chamberlin@edu.univ-fcomte.fr>
2022-05-28 12:16:44 +0200

    commit init

---
+++ src/views/AccueilView.vue
@@ -0,0 +1,29 @@
+<template>
+  <main>
+    <div class="relative flex flex-grow flex-col items-center justify-start gap-5 rounded-[18px] bg-cyan-600 py-2">
+      <img src="djsnake.jpg" class="h-[241px] w-[404.44px] flex-shrink-0 flex-grow-0 rounded-[32px] object-cover" />
+      <p class="h-11 w-[463.78px] flex-shrink-0 flex-grow-0 self-stretch text-center text-3xl text-white"></p>
+      <div
+        class="
+          relative
+          flex
+          w-[466px]
+          flex-shrink-0 flex-grow-0 flex-col
+          items-start
+          justify-start
+          gap-[5px]
+          overflow-hidden
+          bg-cyan-600
+          pb-[5px]
+        "
+      >
+        <span class="h-11 w-[466px] flex-shrink-0 flex-grow-0 text-center text-[25px] font-bold text-white">DJ SNAKE </span><br />
+        <p class="h-11 w-[466px] flex-shrink-0 flex-grow-0 text-center text-[25px] font-bold text-white">EN SAVOIR PLUS</p>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script>
+export default {};
+</script>
