de62bedebd63040a88450964f223e994495a28f0
Morgane <morgane.chamberlin@edu.univ-fcomte.fr>
2022-05-31 11:28:58 +0200

    page concert

--- src/views/ConcertView.vue
+++ src/views/ConcertView.vue
@@ -13,10 +13,10 @@
 
   <img class="h-2/6 w-full" src="src/assets/images/angele-concert.jpg" alt="angele" />
   <div class="ml-auto mr-44 h-96 w-1/5 bg-gray-300">
-    <p class="font-work-sans text-xl font-bold text-black">
+    <p class="py:11 px-12 font-work-sans text-sm font-bold text-black lg:py-28 lg:text-2xl">
       Retrouvez Angele le 1er et le 3 Juillet prochain dans la grande scene Est à 21h pour un concert de dingue
     </p>
-    <div class="m-auto flex w-9 justify-center gap-11">
+    <div class="m-auto flex w-4 justify-center gap-11 lg:w-9">
       <img src="src/assets/icones/spotify.svg" alt="spotify" />
       <img src="src/assets/icones/insta-noir.svg" alt="instagram" />
       <img src="src/assets/icones/twitter-noir.svg" alt="twitter" />
@@ -46,4 +46,49 @@
   </div>
   <p class="m-auto flex justify-center text-2xl">RETROUVEZ D’AUTRES ARTISTES LES MEMES JOURS QU’ELLE !!!</p>
   <div class="m-auto flex h-2 w-3/6 justify-center bg-purple-700"></div>
+
+  <div class="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3">
+    <div>
+      <CardArtist class="mb-10 mt-20" nom="DOJA CAT" image="/src/assets/images/cardib.jpg" />
+    </div>
+    <div>
+      <CardArtist class="mb-10 mt-20" nom="COLDPLAY" image="/src/assets/images/coldplay.jpg" />
+    </div>
+    <div>
+      <CardArtist class="mb-10 mt-20" nom="SIA" image="/src/assets/images/sia.jpg" />
+    </div>
+    <div>
+      <CardArtist class="mb-10" nom="DAVID GUETTA" image="/src/assets/images/davidguetta.jpg" />
+    </div>
+    <div>
+      <CardArtist class="mb-10" nom="ED SHEERAN" image="/src/assets/images/edsheeran.jpg" />
+    </div>
+    <div>
+      <CardArtist class="mb-10" nom="DADDY YANKEE" image="/src/assets/images/daddyyankee.jpg" />
+    </div>
+    <div>
+      <CardArtist class="mb-10" nom="OLIVIA RODRIGO" image="/src/assets/images/oliviarudberg.jpg" />
+    </div>
+    <div>
+      <CardArtist class="mb-10" nom="OMAR RUDBERG" image="/src/assets/images/omarudberg.jpg" />
+    </div>
+    <div>
+      <CardArtist class="mb-10" nom="BIBI" image="/src/assets/images/bibi.jpg" />
+    </div>
+    <div>
+      <CardArtist class="mb-10" nom="CHANMINA" image="/src/assets/images/chanmina.jpg" />
+    </div>
+    <div>
+      <CardArtist class="mb-10" nom="FALLY IPUPA" image="/src/assets/images/faillyipupa.jpg" />
+    </div>
+  </div>
 </template>
+
+<script>
+import CardArtist from "../components/Card.vue";
+export default {
+  components: {
+    CardArtist,
+  },
+};
+</script>
