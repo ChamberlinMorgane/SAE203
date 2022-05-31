a6559f4b8a176fdda27ddf9c4ea2a12fb59f3dac
Morgane <morgane.chamberlin@edu.univ-fcomte.fr>
2022-05-31 08:43:18 +0200

    debut page festival

--- src/views/FestivalView.vue
+++ src/views/FestivalView.vue
@@ -10,4 +10,15 @@
     </div>
     <div class="h-1 w-full bg-white"></div>
   </div>
+
+  <img class="m-auto flex h-2/6 w-3/6 justify-center" src="src/assets/images/plan.jpg" alt="plan du festival" />
+  <p class="m-auto mt-11 flex justify-center text-2xl font-bold">Découvrez nos 3 grandes scene et nos deux petite</p>
+  <p class="m-auto mt-11 flex justify-center text-3xl font-bold">Les grandes scènes</p>
+  <div>
+    <img class="h-1/6 w-2/6" src="src/assets/images/scenenord.jpg" alt="scene nord" />
+    <img class="h-1/6 w-2/6" src="src/assets/images/scenesud.jpg" alt="scene sud" />
+    <img class="h-1/6 w-2/6" src="src/assets/images/sceneest.jpg" alt="scene est" />
+    <img class="h-1/6 w-2/6" src="src/assets/images/scenecentre.jpg" alt="scene centre" />
+    <img class="h-1/6 w-2/6" src="src/assets/images/sceneouest.jpg" alt="scene ouest" />
+  </div>
 </template>
