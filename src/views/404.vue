759d61afcc0a7a303854080209ccecc9bec38e1d
Morgane <morgane.chamberlin@edu.univ-fcomte.fr>
2022-06-01 12:26:10 +0200

    coorection responsive programmation

--- src/views/404.vue
+++ src/views/404.vue
@@ -8,8 +8,50 @@
     <div class="container relative z-10 mx-auto flex items-center px-6 py-32 md:px-12 xl:py-40">
       <div class="relative z-10 flex w-full flex-col items-center font-mono">
         <h1 class="mt-4 text-center text-5xl font-extrabold leading-tight text-white">Tout seul ici</h1>
-        <p class="my-44 animate-bounce text-8xl font-extrabold text-white">404</p>
-        <Bouton nom="Retourne à la page d'accueil" class="animate-bounce border-2 border-white bg-black text-white" />
+        <p class="text-white! my-44 animate-bounce text-8xl font-extrabold">404</p>
+        <form action="#" method="POST">
+          <div class="flex items-center justify-center text-black">
+            <input
+              type="text"
+              name="search"
+              placeholder="What are you looking for?"
+              class="
+                dark:bg-darker
+                w-full
+                rounded-l-md
+                px-4
+                py-2
+                focus:outline-none focus:ring focus:ring-blue-400
+                dark:focus:ring-blue-800
+              "
+            />
+            <button
+              class="
+                focus:ring-primary-light
+                dark:focus:ring-primary-darker
+                rounded-r-md
+                bg-blue-600
+                p-2
+                text-white
+                hover:bg-blue-700
+                focus:outline-none focus:ring
+                dark:bg-blue-700 dark:hover:bg-blue-800
+              "
+            >
+              <span class="sr-only">Recherche</span>
+              <svg
+                aria-hidden="true"
+                class="h-6 w-6"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+              </svg>
+            </button>
+          </div>
+        </form>
       </div>
     </div>
   </div>
--- src/views/ProgrammationView.vue
+++ src/views/ProgrammationView.vue
@@ -16,46 +16,50 @@
   </p>
   <Bouton nom="1 Juillet !!" class="ml-11 flex w-1/6 justify-start text-5xl font-extrabold" />
   <p class="m-auto mb-11 w-2/3 justify-center text-center font-work-sans font-bold text-white">
-    <span class="h-[298.54px] w-[1155.3px] text-center font-work-sans text-6xl font-bold text-white">ED SHEERAN - </span
-    ><span class="h-[298.54px] w-[1155.3px] text-center font-work-sans text-6xl font-bold text-white">ANGELE</span
-    ><span class="h-[298.54px] w-[1155.3px] text-center font-work-sans text-6xl font-bold text-white">-</span
-    ><span class="h-[298.54px] w-[1155.3px] text-center font-work-sans text-7xl font-bold text-white"> </span><br /><span
-      class="h-[298.54px] w-[1155.3px] text-center font-work-sans text-7xl font-bold text-white"
+    <span class="h-[298.54px] w-[1155.3px] text-center font-work-sans text-lg font-bold text-white lg:text-6xl">ED SHEERAN - </span
+    ><span class="h-[298.54px] w-[1155.3px] text-center font-work-sans text-lg font-bold text-white lg:text-6xl">ANGELE</span
+    ><span class="h-[298.54px] w-[1155.3px] text-center font-work-sans text-lg font-bold text-white lg:text-6xl">-</span
+    ><span class="h-[298.54px] w-[1155.3px] text-center font-work-sans text-xl font-bold text-white lg:text-7xl"> </span><br /><span
+      class="h-[298.54px] w-[1155.3px] text-center font-work-sans text-xl font-bold text-white lg:text-7xl"
       >DAVID GUETTA</span
-    ><span class="h-[298.54px] w-[1155.3px] text-center font-work-sans text-6xl font-bold text-white"
+    ><span class="h-[298.54px] w-[1155.3px] text-center font-work-sans text-lg font-bold text-white lg:text-6xl"
       >- OMAR RUDBERG- BIBI- CHANMINA- FALLY IPUPA- COLDPLAY-</span
-    ><span class="h-[298.54px] w-[1155.3px] text-center font-work-sans text-7xl font-bold text-white">DOJA CAT</span
-    ><span class="h-[298.54px] w-[1155.3px] text-center font-work-sans text-6xl font-bold text-white"
+    ><span class="h-[298.54px] w-[1155.3px] text-center font-work-sans text-xl font-bold text-white lg:text-7xl">DOJA CAT</span
+    ><span class="h-[298.54px] w-[1155.3px] text-center font-work-sans text-lg font-bold text-white lg:text-6xl"
       >- OLIVIA RODRIGO- DADDY YANKEE- SIA</span
     >
   </p>
 
-  <Bouton nom="2 Juillet !!" class="float-right mr-11 flex w-1/6 bg-indigo-400 text-5xl font-extrabold" />
+  <Bouton nom="2 Juillet !!" class="float-right mr-11 flex w-1/6 bg-indigo-400 text-xl font-extrabold lg:text-5xl" />
   <p class="m-auto mb-11 w-2/3 justify-center text-center font-bold text-white">
-    <span class="h-[296px] text-center font-work-sans text-7xl font-bold text-white">DJ SNAKE</span
-    ><span class="h-[296px] text-center font-work-sans text-6xl font-bold text-white">- ED SHEERAN- DAVID GUETTA- GIRL IN RED- </span
-    ><span class="h-[296px] text-center font-work-sans text-7xl font-bold text-white">BLACKPINK</span
-    ><span class="h-[296px] text-center font-work-sans text-6xl font-bold text-white">- DIZZY DROSS-</span
-    ><span class="h-[296px] text-center font-work-sans text-6xl font-bold text-white"> COLDPLAY</span
-    ><span class="h-[296px] text-center font-work-sans text-6xl font-bold text-white">- OLIVIA RODRIGO- </span
-    ><span class="h-[296px] text-center font-work-sans text-6xl font-bold text-white">DADDY YANKEE</span
-    ><span class="h-[296px] text-center font-work-sans text-6xl font-bold text-white">- TONES AND I- SIA</span>
+    <span class="h-[296px] text-center font-work-sans text-xl font-bold text-white lg:text-7xl">DJ SNAKE</span
+    ><span class="h-[296px] text-center font-work-sans text-lg font-bold text-white lg:text-6xl"
+      >- ED SHEERAN- DAVID GUETTA- GIRL IN RED- </span
+    ><span class="h-[296px] text-center font-work-sans text-xl font-bold text-white lg:text-7xl">BLACKPINK</span
+    ><span class="h-[296px] text-center font-work-sans text-lg font-bold text-white lg:text-6xl">- DIZZY DROSS-</span
+    ><span class="h-[296px] text-center font-work-sans text-lg font-bold text-white lg:text-6xl"> COLDPLAY</span
+    ><span class="h-[296px] text-center font-work-sans text-lg font-bold text-white lg:text-6xl">- OLIVIA RODRIGO- </span
+    ><span class="h-[296px] text-center font-work-sans text-lg font-bold text-white lg:text-6xl">DADDY YANKEE</span
+    ><span class="lg:text-6x text-lgl h-[296px] text-center font-work-sans font-bold text-white">- TONES AND I- SIA</span>
   </p>
 
-  <Bouton nom="3 Juillet !!" class="ml-11 flex w-1/6 justify-end bg-pink-400 text-5xl font-extrabold" />
+  <Bouton nom="3 Juillet !!" class="ml-11 flex w-1/6 justify-end bg-pink-400 text-xl font-extrabold lg:text-5xl" />
   <p class="m-auto mb-11 w-2/3 justify-center text-center font-bold text-white">
-    <span class="h-[296px] text-center font-work-sans text-[50px] font-bold text-white">DJ SNAKE- </span
-    ><span class="h-[296px] text-center font-work-sans text-[80px] font-bold text-white">STROMAE</span
-    ><span class="h-[296px] text-center font-work-sans text-[50px] font-bold text-white">- ANGELE- DAVID GUETTA- </span
-    ><span class="h-[296px] text-center font-work-sans text-6xl font-bold text-white">BLACKPINK</span
-    ><span class="h-[296px] text-center font-work-sans text-[50px] font-bold text-white"
+    <span class="h-[296px] text-center font-work-sans text-lg font-bold text-white lg:text-6xl">DJ SNAKE- </span
+    ><span class="h-[296px] text-center font-work-sans text-xl font-bold text-white lg:text-7xl">STROMAE</span
+    ><span class="h-[296px] text-center font-work-sans text-lg font-bold text-white lg:text-6xl">- ANGELE- DAVID GUETTA- </span
+    ><span class="h-[296px] text-center font-work-sans text-lg font-bold text-white lg:text-6xl">BLACKPINK</span
+    ><span class="h-[296px] text-center font-work-sans text-lg font-bold text-white lg:text-6xl"
       >- DOJA CAT- SABRINA CARPENTER-SHAKIRA DADDY YANKEE- </span
-    ><span class="h-[296px] text-center font-work-sans text-6xl font-bold text-white">TONES AND I</span
+    ><span class="h-[296px] text-center font-work-sans text-lg font-bold text-white lg:text-6xl">TONES AND I</span
     ><span class="h-[296px] text-center text-[50px] font-bold text-white">- </span
-    ><span class="h-[296px] text-center font-work-sans text-[80px] font-bold text-white">SIA</span>
+    ><span class="h-[296px] text-center font-work-sans text-xl font-bold text-white lg:text-7xl">SIA</span>
   </p>
 
-  <Bouton nom="Achetez vos billet dès maintenant" class="m-auto mt-32 flex w-3/6 justify-center border-4 border-black bg-white text-5xl" />
+  <Bouton
+    nom="Achetez vos billet dès maintenant"
+    class="m-auto mt-32 flex w-3/6 justify-center border-4 border-black bg-white text-xl lg:text-5xl"
+  />
   <div class="m-auto my-32 flex justify-center gap-11">
     <a href="artistes"><img class="w-16" src="/icones/arrow-narrow-up.svg" alt="flèche" /></a>
     <a href="artistes"><p class="text-2xl font-bold">Découvrez la liste complet des artistes présent au festival</p></a>
